c935a3d3744e856c2bc396330283059a86788c3d
Kevin Wu <kevin@autoaccru.com>
2026-08-03 16:49:11 -0400

    deploy webapp

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
 // A fixed name meant an installed app could keep serving the old shell
 // from cache and there was no way to tell it otherwise short of
 // deleting the app.
-const CACHE = 'chinese-5531e26faa';
+const CACHE = 'chinese-37416076b9';
 const SHELL = ['./', './index.html', './srs.js', './config.json',
                './manifest.webmanifest', './icon-180.png', './icon-512.png'];
 
